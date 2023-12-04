5de3bf73a497926627c32ccc02f620c7209c2729
Paulabm24 <114907123+Paulabm24@users.noreply.github.com>
2023-12-04 13:56:08 +0100

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 apt update
 apt install apache2
-systemctl status apache2
-cd /var/www/html
-ls
+apt install mariadb-server
 apt install php
 apt install phpmyadmin
-systemctl status phpmyadmin
-apt install mariadb-server
-systemctl status mariadb-server
-#accedemos con la ip publica a phpmyadmin en el navegador
 
 
 
